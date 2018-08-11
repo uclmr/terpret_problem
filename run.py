8ea917affbea3ea0837687dfb45581d67a94de17
Edward Grefenstette <etg@google.com>
2018-08-11 14:50:49 +0100

    Added stochastic training and entropy regulariser.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import time
 import tensorflow as tf
 from options import parse_options
 from util import SharedLogDirichletInitializer
@@ -14,10 +15,19 @@ if __name__ == "__main__":
         tp = TerpretProblem(opts)
         tf.global_variables_initializer().run(session=sess)
 
+        for k, v in opts.__dict__.items():
+            print(k, ":", v)
+        sys.stdout.flush()
+        time.sleep(3)
+
         for epoch in range(opts.n_epochs):
             _, loss = sess.run([tp.update, tp.loss])
-            sys.stdout.write("[%d] %.8f" % (epoch, loss))
-            mus = sess.run(tp.mus)
+            mus, mus_stoch = sess.run([tp.mus, tp.mus_stoch])
+            sys.stdout.write("[%d] %.8f D: " % (epoch, loss))
             for i in range(opts.v):
-                sys.stdout.write("%d " % round(100 * mus[i, 0]))
+                sys.stdout.write("%03d " % round(100 * mus[i, 0]))
             sys.stdout.write("\n")
+            sys.stdout.write("[%d] %.8f S: " % (epoch, loss))
+            for i in range(opts.v):
+                sys.stdout.write("%03d " % round(100 * mus_stoch[i, 0]))
+            sys.stdout.write("\n\n")
--- terpret_problem.py
+++ terpret_problem.py
@@ -1,5 +1,6 @@
 import argparse
 import tensorflow as tf
+import tensorflow_probability as tfp
 import numpy as np
 import sys
 from util import SharedLogDirichletInitializer, MaxEntInitializer
@@ -26,13 +27,17 @@ class TerpretProblem:
         # mus : T [k, 2]
         # These are the parameters for the distributions of the full xs chain.
         self.mus = tf.concat([self.mu0, tf.nn.softmax(self.ms)], axis=0)
+        self.mus_stoch = tf.concat([self.mu0, tfp.distributions.RelaxedOneHotCategorical(opts.temp, self.ms).sample()], axis=0)
+
+        self.mus_opt = self.mus_stoch if opts.stochastic else self.mus
 
         # Returns the probability that `xor(xs[i], xs[i+1%k])` equals `0`
         def soft_xor_p0(i):
             j = tf.mod(i+1, opts.k)
-            return (self.mus[i, 0] * self.mus[j, 0]) + (self.mus[i, 1] * self.mus[j, 1])
+            return (self.mus_opt[i, 0] * self.mus_opt[j, 0]) + (self.mus_opt[i, 1] * self.mus_opt[j, 1])
 
         # ys_eq_0 : T [k]
         self.ys_eq_0 = tf.map_fn(soft_xor_p0, tf.range(opts.k), dtype=tf.float32)
-        self.loss = - tf.reduce_sum(tf.log(self.ys_eq_0))
+        self.entropy = tf.reduce_sum(-tf.log(tf.nn.softmax(self.ms)) * tf.nn.softmax(self.ms))
+        self.loss = - tf.reduce_sum(tf.log(self.ys_eq_0)) + opts.entropy_weight * self.entropy
         self.update = tf.train.AdamOptimizer(learning_rate=opts.learning_rate).minimize(self.loss)
